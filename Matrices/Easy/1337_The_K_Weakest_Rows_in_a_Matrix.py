7946f243e0c4722f2e035872424aba769c115f0c
Pranik Privi <praprivi27@gmail.com>
2021-01-03 04:33:20 +0200

    Initial Commit

---
+++ Matrices/Easy/1337_The_K_Weakest_Rows_in_a_Matrix.py
@@ -0,0 +1,93 @@
+'''
+
+Given a m * n matrix mat of ones (representing soldiers) and zeros (representing civilians), return the indexes of the k weakest rows in the matrix ordered from the weakest to the strongest.
+
+A row i is weaker than row j, if the number of soldiers in row i is less than the number of soldiers in row j, or they have the same number of soldiers but i is less than j. Soldiers are always stand in the frontier of a row, that is, always ones may appear first and then zeros.
+
+
+
+Example 1:
+
+Input: mat =
+[[1,1,0,0,0],
+ [1,1,1,1,0],
+ [1,0,0,0,0],
+ [1,1,0,0,0],
+ [1,1,1,1,1]],
+k = 3
+Output: [2,0,3]
+Explanation:
+The number of soldiers for each row is:
+row 0 -> 2
+row 1 -> 4
+row 2 -> 1
+row 3 -> 2
+row 4 -> 5
+Rows ordered from the weakest to the strongest are [2,0,3,1,4]
+Example 2:
+
+Input: mat =
+[[1,0,0,0],
+ [1,1,1,1],
+ [1,0,0,0],
+ [1,0,0,0]],
+k = 2
+Output: [0,2]
+Explanation:
+The number of soldiers for each row is:
+row 0 -> 1
+row 1 -> 4
+row 2 -> 1
+row 3 -> 1
+Rows ordered from the weakest to the strongest are [0,2,3,1]
+
+
+Constraints:
+
+m == mat.length
+n == mat[i].length
+2 <= n, m <= 100
+1 <= k <= m
+matrix[i][j] is either 0 or 1.
+
+Time Complexity: O(nlogn)
+Space Complexity: O(n) where n is the no of rows of the Matrix
+'''
+
+
+class Solution(object):
+    def kWeakestRows(self, mat, k):
+        """
+        :type mat: List[List[int]]
+        :type k: int
+        :rtype: List[int]
+        """
+
+        soldier = []
+        res = []
+
+        for idx, vect in enumerate(mat):
+
+            if vect[0] == 0:
+                soldier.append([0, idx])
+                continue
+
+            left = 0
+            right = len(vect) - 1
+
+            while left < right:
+                mid = (right + left + 1) // 2
+
+                if vect[mid]:
+                    left = mid
+                else:
+                    right = mid - 1
+
+            soldier.append([left + 1, idx])
+
+        soldier.sort()
+
+        for i in range(k):
+            res.append(soldier[i][1])
+
+        return res
